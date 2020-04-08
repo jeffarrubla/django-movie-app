f9cee811330e80cd853ca9f4db15dee78a556df8
Jefferson <jeffarrubla@gmail.com>
2020-04-08 16:56:12 +0000

    Creating the view and template

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Movies
 # Create your views here.
+
+def movie_list(request):
+	movies_objects = Movies.objects.all()
+	return render(request,'newapp/movie_list.html',{'movies_objects':movies_objects})
